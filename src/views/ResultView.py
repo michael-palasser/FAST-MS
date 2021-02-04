a7d1cbe6380c08207270d52cbf4d4f8e94839af8
Michael Palasser <michael.palasser@uibk.ac.at>
2021-02-04 12:08:43 +0100

    03.02.21 finished TableView

--- src/views/ResultView.py
+++ src/views/ResultView.py
@@ -6,23 +6,34 @@ import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt, QSortFilterProxyModel
 import pandas as pd
+from PyQt5.QtGui import QStandardItem
+from PyQt5.QtWidgets import QAbstractItemView
 
 from src.entities.Ions import FragmentIon, Fragment
 from src.views.SpectrumView import SpectrumView
 
 
-class TableModel(QtCore.QAbstractTableModel):
+class IonTableModel(QtCore.QAbstractTableModel):
     def __init__(self, data):
-        super(TableModel, self).__init__()
+        super(IonTableModel, self).__init__()
         self._data = data
         #self._format = ['{:10.5f}','{:2d}', '{:12d}', '','{:4.2f}', '{:6.1f}', '{:4.2f}', '']
-        self._format = ['{:10.5f}','{:2d}', '{:12d}', '','{:4.2f}', '{:6.1f}', '{:4.2f}', '']
+        self._format = ['{:10.5f}','{:2d}', '{:12d}', '','{:4.2f}', '{:6.1f}', '{:4.2f}', '{:4.2f}', '']
+        """for i, header in enumerate(('m/z','z','intensity','fragment','error /ppm', 'S/N','quality',
+                                                        'formula','score', 'comment')):
+            self.setHeaderData(i, Qt.Horizontal, header)"""
+        self._headers = ('m/z','z','intensity','fragment','error /ppm', 'S/N','quality', 'score', 'comment')
+        self.setHeaderData(0, QtCore.Qt.Horizontal, "Test")
+
+    """def flags(self, index):
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable"""
 
     def data(self, index, role):
         if index.isValid():
             if role == Qt.DisplayRole:
                 col = index.column()
-                item = self._data.values[index.row()][col]
+                #item = self._data.values[index.row()][col]
+                item = self._data[index.row()][col]
                 formatString = self._format[col]
                 if col == 3 or col == 7:
                     return item
@@ -34,39 +45,115 @@ class TableModel(QtCore.QAbstractTableModel):
                 return formatString.format(item)
 
     def rowCount(self, index):
+        #return len(self._data.values)
+        return len(self._data)
+
+    def columnCount(self, index):
+        #return self._data.columns.size
+        return len(self._data[0])
+
+    def headerData(self, section, orientation, role):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self._headers[section]
+
+    def getHashOfRow(self, rowIndex):
+        #nameIndex = self.index(rowIndex, 3)
+        #chargeIndex = self.index(rowIndex, 1)
+        #return self._data.index[rowIndex]
+        return (self._data[rowIndex][3],self._data[rowIndex][1])
+
+    def sort(self, Ncol, order):
+        """Sort table by given column number."""
+        self.layoutAboutToBeChanged.emit()
+        print(Ncol)
+        #self._data = self._data.sort_values(self._headers[Ncol], ascending=order == Qt.AscendingOrder)
+        if order == Qt.AscendingOrder:
+            self._data.sort(key= lambda tup:tup[Ncol])
+        else:
+            self._data.sort(key= lambda tup:tup[Ncol], reverse=True)
+        self.layoutChanged.emit()
+
+
+
+    def addData(self, newRow):
+        self._data.append(newRow)
+
+    def removeData(self, indexToRemove):
+        del self._data[indexToRemove]
+
+
+
+
+class PeakTableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(PeakTableModel, self).__init__()
+        self._data = data
+        #self._format = ['{:10.5f}','{:2d}', '{:12d}', '','{:4.2f}', '{:6.1f}', '{:4.2f}', '']
+        self._format = ['{:10.5f}','{:2d}', '{:11d}', '','{:4.2f}', '{:6.1f}', '{:4.2f}', '']
+        self.setHeaderData(0, QtCore.Qt.Horizontal, "Test")
+
+    def flags(self, index):
+        return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+    def data(self, index, role):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                col = index.column()
+                item = self._data.values[index.row()][col]
+                formatString = self._format[col]
+                if col == 3:
+                    return item
+                if col == 2 :
+                    if item >= 10 ** 12:
+                        lg10 = str(int(log10(item) + 1))
+                        formatString = '{:' + lg10 + 'd}'
+                    return formatString.format(item)
+                return formatString.format(item)
+
+    def rowCount(self, index):
         return len(self._data.values)
 
     def columnCount(self, index):
         return self._data.columns.size
 
-
-
+    def headerData(self, section, orientation, role):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return ('m/z','z','intensity','fragment','error /ppm', 'used')[section]
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, ions, spectrum):
+    def __init__(self, ions, deletedIons, spectrum):
         super().__init__()
-        self._ions = ions
-        self._spectrum = spectrum
-
-        self.table = QtWidgets.QTableView()
-        valList, indizes = [], []
-        for ion in pattern:
-            valList.append(ion.getValues()+[ion.comment])
+        #self._ions = ions
+        #self._deletedIons = deletedIons
+        #self._spectrum = spectrum
+        #{self.hash(ion): ion for ion in ions}
+        """valList, indizes = [], []
+        for ion in ions:
+            valList.append(ion.getMoreValues())
             indizes.append(self.hash(ion))
         shownData = pd.DataFrame(data=valList, columns=['mz','z','int','name','error','snr','qual','com'],
-                                 index=pd.MultiIndex.from_tuples(indizes))
-        print(shownData)
-        self.model = TableModel(shownData)
-        self.table.setModel(self.model)
+                                 index=pd.MultiIndex.from_tuples(indizes))"""
+        #self.table = self.makeTable(shownData)
+        """self.table = self.makeTable(self._ions)
+        self.delTable = self.makeTable(self._deletedIons)"""
+        self.tables = [self.makeTable(ions), self.makeTable(deletedIons)]
+        self.setCentralWidget(self.tables[0])
+
+    def makeTable(self, data):
+        model = IonTableModel([ion.getMoreValues() for ion in data.values()])
         self.proxyModel = QSortFilterProxyModel()
-        self.proxyModel.setSourceModel(self.model)
+        self.proxyModel.setSourceModel(model)
+        table = QtWidgets.QTableView()
+        table.setModel(model)
+        table.setSortingEnabled(True)
+        #table.setModel(self.proxyModel)
+        self.connectTable(table)
+        table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        return table
 
-        self.table.setSortingEnabled(True)
-
-        self.table.setModel(self.proxyModel)
-
-        self.setCentralWidget(self.table)
 
     def connectTable(self, table):
         table.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
@@ -77,35 +164,73 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def showOptions(self, table, pos):
         global view
-        """it = table.itemAt(pos)
-        if it is None:
-            return
-        selectedRowIndex = it.row()
-        columnCount = table.columnCount()
-        item_range = QtWidgets.QTableWidgetSelectionRange(0, selectedRowIndex, columnCount - 1, selectedRowIndex)
-        table.setRangeSelected(item_range, True)"""
         menu = QtWidgets.QMenu()
         showAction = menu.addAction("Show in Spectrum")
-        delAction = menu.addAction("Delete/Undelete")
+        peakAction = menu.addAction("Show Peaks")
+        actionStrings = ["Delete", "Restore"]
+        mode = 0
+        other = 1
+        if table != self.table:
+            mode = 1
+            other = 0
+        delAction = menu.addAction(actionStrings[mode])
         action = menu.exec_(table.viewport().mapToGlobal(pos))
+        it = table.indexAt(pos)
+        if it is None:
+            return
+        selectedRow = it.row()
+        selectedHash = table.model().getHashOfRow(selectedRow)
         if action == showAction:
-            """index = None
-            for i, t in enumerate(self._tables):
-                if t == table:
-                    index = i
-            minLimit = 4000
-            maxLimit = 0
-            for ion in self._patterns[index].values():
-                minMz = np.min(ion.isotopePattern['m/z'])
-                maxMz = np.max(ion.isotopePattern['m/z'])
-                if minLimit > minMz:
-                    minLimit = minMz
-                if maxLimit < maxMz:
-                    maxLimit = maxMz"""
-            ions = self.getIons(table)
-            minLimit, maxLimit, YLimit = self.getLimits(ions)
+            ions = self.getIons(self._ions[selectedHash])
+            print(ions)
+            minLimit, maxLimit, maxY = self.getLimits(ions)
             peaks = self._spectrum[np.where((self._spectrum[:,0]>(minLimit-5)) & (self._spectrum[:,0]<(maxLimit+5)))]
-            view = SpectrumView(peaks, ions, minLimit, maxLimit, YLimit)
+            view = SpectrumView(peaks, ions, np.min(self._ions[selectedHash].isotopePattern['m/z']),
+                                np.max(self._ions[selectedHash].isotopePattern['m/z']),
+                                np.max(self._ions[selectedHash].isotopePattern['relAb']))
+        elif action == peakAction:
+            pass
+            #self.peakView = PeakView(self._ions[selectedHash].getPeaks())
+            """print(ions)
+            minLimit, maxLimit, maxY = self.getLimits(ions)
+            peaks = self._spectrum[np.where((self._spectrum[:,0]>(minLimit-5)) & (self._spectrum[:,0]<(maxLimit+5)))]
+            view = SpectrumView(peaks, ions, np.min(self._ions[selectedHash].isotopePattern['m/z']),
+                                np.max(self._ions[selectedHash].isotopePattern['m/z']),
+                                np.max(self._ions[selectedHash].isotopePattern['relAb']))"""
+        elif action == delAction:
+            #actionString = actionString[:-1]+'ing '
+            choice = QtWidgets.QMessageBox.question(self, "",
+                                        actionStrings[mode][:-1]+'ing '+self._ions[selectedHash].getName()+"?",
+                                        QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+            if choice == QtWidgets.QMessageBox.Yes:
+                ionLists = [self._ions, self._deletedIons]
+                comments = ['man.del.','man.undel.']
+                ionToDelete = ionLists[mode][selectedHash]
+                ionToDelete.comment += comments[mode]
+                ionLists[other][selectedHash] = ionToDelete
+                del ionLists[mode][selectedHash]
+
+                #self.table.model().removeRow(selectedRow)
+                table.model().removeData(selectedRow)
+                table.model().addData(ion.getMoreValues())
+                print('deleted',selectedRow, selectedHash)
+
+
+
+class PeakView(QtWidgets.QWidget):
+    def __init__(self, peaks):
+        super().__init__()
+        self._peaks = peaks
+        model =PeakTableModel(peaks)
+        self.proxyModel = QSortFilterProxyModel()
+        self.proxyModel.setSourceModel(model)
+        self.table = QtWidgets.QTableView()
+        self.table.setSortingEnabled(True)
+        self.table.setModel(self.proxyModel)
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.show()
+
+
 
 
 
@@ -121,7 +246,9 @@ for i, ion in enumerate(pattern):
     ion.noise = 10 ** 6 * 2
     print(ion.getValues())
 
+
 app=QtWidgets.QApplication(sys.argv)
-window=MainWindow(pattern)
+window=MainWindow({(ion.getName(),ion.charge): ion for ion in pattern},
+                  {(ion.getName(),ion.charge): ion for ion in pattern}, [])
 window.show()
 app.exec_()

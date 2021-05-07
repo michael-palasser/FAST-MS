07.05.21 tests for fastFunctions.py, MolecularFormula.py: silent bugs fixed concerning calculating isotope table for universal elemental composition (e.g. for formula C5N5H5OK0, value 0 made problems)

src/MolecularFormula.py
@@ -98,6 +98,11 @@ class MolecularFormula(object):
                 returnedString += element + str(self._formulaDict[element])
         return returnedString
 
+    def determineSystem(self, elements, theElements):
+        for elem in elements:
+            if elem not in theElements:
+                return False
+        return True
 
     def calculateIsotopePattern(self):
         '''
@@ -108,10 +113,11 @@ class MolecularFormula(object):
         prop = 1
         isoPeak=0
         isotope_pattern = list()
-        if self._formulaDict.keys() == {'C', 'H', 'N', 'O', 'P'}:
+        elements = [key for key,val in self._formulaDict.items() if val>0]
+        if self.determineSystem(elements, {'C', 'H', 'N', 'O', 'P'}):
             calculate = calculateNuclFineStructure
             isotopeTable = self.makeNucIsotopeTable()
-        elif self._formulaDict.keys() == {'C', 'H', 'N', 'O', 'S'}:
+        elif self.determineSystem(elements, {'C', 'H', 'N', 'O', 'S'}):
             calculate = calculatePeptFineStructure
             isotopeTable = self.makeProteinIsotopeTable()
         else:
@@ -124,7 +130,6 @@ class MolecularFormula(object):
             prop = np.sum(ultrafineStruct[:,1])
             M_iso = np.sum(ultrafineStruct[:,0]*ultrafineStruct[:,1])/prop
             isotope_pattern.append((M_iso,prop))
-            print(isoPeak, prop, prop/mostAbundant)
             isoPeak += 1
             if prop > mostAbundant:
                 mostAbundant = prop
@@ -178,20 +183,20 @@ class MolecularFormula(object):
             mass of isotope, nominal mass shift compared to isotope with lowest m/z)
         '''
         isotopeTable = list()
-        for index, elem in enumerate(sorted(list(self._periodicTable.keys()))):
+        elements = [elem for elem,val in self._periodicTable.items() if self._formulaDict[elem] > 0]
+        #for index, elem in enumerate(sorted(list(self._periodicTable.keys()))):
+        for index, elem in enumerate(sorted(elements)):
             mono = self._periodicTable[elem][0][0]
             for isotope in self._periodicTable[elem]:
-                if self._formulaDict[elem] != 0:
-                    isotopeTable.append((index, self._formulaDict[elem], 0, isotope[2], isotope[1], isotope[0] - mono))
+                #if self._formulaDict[elem] != 0:
+                isotopeTable.append((index, self._formulaDict[elem], 0, isotope[2], isotope[1], isotope[0] - mono))
         isotopeTable = np.array(sorted(isotopeTable,key=lambda tup: tup[1], reverse=True)
                                 , dtype=[('index', np.float64), ('nr', np.float64), ('nrIso', np.float64),
                                          ('relAb', np.float64), ('mass', np.float64), ('M+', np.float64)])
         """isotopeTable = np.array(isotopeTable
                                 , dtype=[('index', np.float64), ('nr', np.float64), ('nrIso', np.float64),
                                          ('relAb', np.float64), ('mass', np.float64), ('M+', np.float64)])"""
-        print('len',len(getByIndex(isotopeTable, isotopeTable['index'][0])),isotopeTable)
         if len(getByIndex(isotopeTable, isotopeTable['index'][0])) != 2:
-            print('oh no', isotopeTable)
             match = None
             for isotope in isotopeTable:
                 if len(getByIndex(isotopeTable, isotope['index'])) == 2:
@@ -206,7 +211,6 @@ class MolecularFormula(object):
                     oldIndex = row['index']
                     index += 1
                 row['index'] = index
-        print('hey ho', isotopeTable)
         return isotopeTable
 
     def calculateMonoIsotopic(self):

src/fastFunctions.py
@@ -16,6 +16,9 @@ def getByIndex(isotopeTable, index):
     :param (int) index: index
     :return: ndarray(dtype=[float,float,float,float,float,float]) rows of table with corresponding indices
     '''
+    '''if len(isotopeTable[np.where(isotopeTable['index'] == index)]) == 0:
+        print(index)
+        raise Exception(str(index))'''
     return isotopeTable[np.where(isotopeTable['index'] == index)]
 
 
@@ -127,7 +130,7 @@ def calculateNuclFineStructure(isotopePeak, isotopeTable):
         isotope, mass of isotope, nominal mass shift compared to isotope with lowest m/z)
     :return: (ndarray(dtype=[float,float])) fine structure [(mass,rel.abundance)]
     '''
-    print(isotopePeak)
+    #print(isotopePeak)
     maxValues = getMaxValues(isotopePeak, isotopeTable)
     """for i in range(10):
         if isotopePeak>isotopeTable[i]['nr']:
@@ -136,7 +139,7 @@ def calculateNuclFineStructure(isotopePeak, isotopeTable):
             maxValues.append(isotopePeak)
     if isotopePeak > 0:
         isotopeTable[1]['nrIso'] = isotopePeak"""
-    print(isotopeTable['nrIso'])
+    #print(isotopeTable['nrIso'])
     massI, propI = calculatePercentage(isotopeTable)
     fineStructure = [(massI, propI)]
     for i13C in list(range(maxValues[1]))[::-1]:
@@ -147,7 +150,7 @@ def calculateNuclFineStructure(isotopePeak, isotopeTable):
                         if (i13C + i2H + i15N + i17O + 2 * i18O == isotopePeak) and \
                                 not (i17O + i18O > isotopeTable[6]['nr']) :
                             nrIsoList= np.array([0.,i13C,0.,i2H,0.,i15N,0.,i17O,i18O,0.])
-                            print(nrIsoList)
+                            #print(nrIsoList)
                             for i in range(len(isotopeTable)):
                                 isotopeTable[i]['nrIso']=nrIsoList[i]
                             #massI, propI = calculatePercentage(isotopeTable)
@@ -208,7 +211,7 @@ def calculatePeptFineStructure(isotopePeak, isotopeTable):
             maxValues.append(isotopePeak)
     if isotopePeak > 0:
         isotopeTable[1]['nrIso'] = isotopePeak"""
-    print(isotopeTable['nrIso'])
+    #print(isotopeTable['nrIso'])
     massI, propI = calculatePercentage(isotopeTable)
     fineStructure = [(massI, propI)]
     for i13C in list(range(maxValues[1]))[::-1]:
@@ -256,14 +259,14 @@ def calculateFineStructure(isotopePeak, isotopeTable):
         isotope, mass of isotope, nominal mass shift compared to isotope with lowest m/z)
     :return: (ndarray(dtype=[float,float])) fine structure [(mass,percentage)]
     '''
-    print(isotopePeak)
+    #print(isotopePeak)
     fineStructure = [(0.,0.)]
     for iFirst in list(range(isotopePeak + 1))[::-1]:
         isotopeTable[1]['nrIso'] = iFirst
         #print('first',iFirst)
         if iFirst == isotopePeak:
             massI, propI = calculatePercentage(isotopeTable)
-            print(isotopeTable['nrIso'])
+            #print(isotopeTable['nrIso'])
             fineStructure.append((massI,propI))
         else:
             fineStructure = loopThroughIsotopes(isotopePeak, isotopeTable, fineStructure, 3)
@@ -274,17 +277,17 @@ def calculateFineStructure(isotopePeak, isotopeTable):
 
 @njit
 def checkIsotopeTable(isotopeTable):
-    for i in range(np.max(isotopeTable['index'])+1):
+    for i in range(int(np.max(isotopeTable['index'])+1)):
         currentElements = getByIndex(isotopeTable,i)
         if np.sum(currentElements['nrIso'])> currentElements['nr'][0]:
-            print('no',isotopeTable)
+            #print('no',isotopeTable)
             return False
     return True
 
 @njit
 def loopThroughIsotopes(isotopePeak, isotopeTable, fineStructure, index):
     '''
-    Recursive function which loops through molecular formula and calculates isotopic fine structure (recursive function)
+    Recursive function which loops through molecular formula and calculates isotopic fine structure (recursive function):
     :param (int) isotopePeak: number of isotope peak (M+x)
     :type isotopeTable: ndarray(dtype=[float,float,float,float,float,float])
     :param isotopeTable: isotope table (index, nr. of atoms of element, nr. of atoms of isotope, rel. abundance of
@@ -293,28 +296,36 @@ def loopThroughIsotopes(isotopePeak, isotopeTable, fineStructure, index):
     :param (int) index: running index of corresponding element
     :return: (list[tuple[float,float]]) final fineStructure [(mass,rel.abundance)]
     '''
+    #If final isotope table for one isotope composition found
     if (np.sum(isotopeTable['nrIso']*isotopeTable['M+']) == isotopePeak):
+        #if combined nuumber of isotopes of element is not higher than nr of corresponding element atoms in molecule
         if checkIsotopeTable(isotopeTable):
             massI, propI = calculatePercentage(isotopeTable)
             fineStructure.append((massI, propI))
-            print(isotopePeak, isotopeTable)
         return fineStructure #eig sinnlos
+    #If isotope table unfinished
     elif index < len(isotopeTable):
         '''for i in range(int((isotopePeak + isotopeTable[index]['M+']) / isotopeTable[index]['M+'])):
             if i>isotopeTable[index]['nr']:
                 break
             isotopeTable[index]['nrIso'] = i
             loopThroughIsotopes(isotopePeak, isotopeTable, fineStructure, index + 1)'''
+        #if current isotope in table is highest abundant isotope loop until M+x (x>=1) isotope is current isotope
         while (isotopeTable[index]['M+'] == 0):
             index+=1
+            #if end of table is reached
             if index >= len(isotopeTable):
                 #print('exit')
                 return fineStructure
         currentIso = isotopeTable[index]
+        #loop over all possible
         for nrIso in range(int((isotopePeak + currentIso['M+']) / currentIso['M+'])):
+            #break if nr of isotope atoms is higher than nr of corresponding atoms of element in molecule
             if nrIso>currentIso['nr']:
                 break
+            #set nr of current isotope in table
             isotopeTable[index]['nrIso'] = nrIso
+            #loop to next isotope in table
             loopThroughIsotopes(isotopePeak, isotopeTable, fineStructure, index + 1)
     return fineStructure
 

tests/test_MolecularFormula.py
@@ -6,13 +6,14 @@ from src.fastFunctions import getByIndex
 from src.MolecularFormula import MolecularFormula
 
 molFormulaDummy = MolecularFormula('C5H4N3O')
-RNA_formulaDummy = MolecularFormula('C38H48N15O26P3') #GCAU
-peptideFormulaDummy = MolecularFormula('C36H56N12O14S')  #GCASDQHPV
+RNA_formulaDummy = MolecularFormula('C38H48N15O26P3')  # GCAU
+peptideFormulaDummy = MolecularFormula('C36H56N12O14S')  # GCASDQHPV
 uniFormulaDummy = MolecularFormula('C5H5N5ONa')
 
-dict0= {'C':5,'H':4,'N':3, 'O':1}
-dict1= {'C':5,'H':4,'N':3, 'O':1, 'S': 10}
-dict2= {'C':10,'H':8,'N':6, 'O':2, 'S': 10}
+dict0 = {'C': 5, 'H': 4, 'N': 3, 'O': 1}
+dict1 = {'C': 5, 'H': 4, 'N': 3, 'O': 1, 'S': 10}
+dict2 = {'C': 10, 'H': 8, 'N': 6, 'O': 2, 'S': 10}
+
 
 class MolecularFormulaTest(TestCase):
 
@@ -24,25 +25,43 @@ class MolecularFormulaTest(TestCase):
 
     def test_subtract_formula(self):
         molFormula2 = MolecularFormula(dict1)
-        self.assertEqual({'C':0,'H':0,'N':0, 'O':0, 'S': 10}, molFormula2.subtractFormula(dict0).getFormulaDict())
+        self.assertEqual({'C': 0, 'H': 0, 'N': 0, 'O': 0, 'S': 10}, molFormula2.subtractFormula(dict0).getFormulaDict())
 
     def test_multiply_formula(self):
-        self.assertEqual(dict2, molFormulaDummy.multiplyFormula(2).addFormula({'S':10}).getFormulaDict())
+        self.assertEqual(dict2, molFormulaDummy.multiplyFormula(2).addFormula({'S': 10}).getFormulaDict())
 
     def test_check_for_negative_values(self):
-        self.assertTrue(molFormulaDummy.subtractFormula({'S':1}))
+        self.assertTrue(molFormulaDummy.subtractFormula({'S': 1}))
 
     '''def test_to_string(self):
         self.fail()'''
 
+    def test_determine_system(self):
+        molFormula  = MolecularFormula('C5H4N3O')
+        elements= [key for key,val in molFormula.getFormulaDict().items() if val>0]
+        self.assertTrue(molFormula.determineSystem(elements, {'C','H','N','O','P'}))
+        molFormula  = MolecularFormula('C5H4N3OS0')
+        elements= [key for key,val in molFormula.getFormulaDict().items() if val>0]
+        self.assertTrue(molFormula.determineSystem(elements, {'C','H','N','O','P'}))
+        molFormula  = MolecularFormula('C38H48N15O26P3')
+        elements= [key for key,val in molFormula.getFormulaDict().items() if val>0]
+        self.assertTrue(molFormula.determineSystem(elements, {'C','H','N','O','P'}))
+        molFormula  = MolecularFormula('C36H56N12O14S')
+        elements= [key for key,val in molFormula.getFormulaDict().items() if val>0]
+        self.assertTrue(molFormula.determineSystem(elements, {'C','H','N','O','S'}))
+        molFormula  = MolecularFormula('C5H5N5ONa')
+        elements= [key for key,val in molFormula.getFormulaDict().items() if val>0]
+        self.assertFalse(molFormula.determineSystem(elements, {'C','H','N','O','P'}))
+        self.assertFalse(molFormula.determineSystem(elements, {'C','H','N','O','S'}))
 
-    def testIsotopeTable(self, isotopeTable = None, values = None):
-        if type(isotopeTable) != type(None) :
+
+    def testIsotopeTable(self, isotopeTable=None, values=None):
+        if type(isotopeTable) != type(None):
             self.assertEqual(len(isotopeTable), 9)
             self.assertEqual(len(isotopeTable[0]), 6)
             for row in isotopeTable:
-                self.assertAlmostEqual(0,row['nrIso'])
-                self.assertAlmostEqual(values[int(row['index'])],row['nr'])
+                self.assertAlmostEqual(0, row['nrIso'])
+                self.assertAlmostEqual(values[int(row['index'])], row['nr'])
 
     def sortByMass(self, array):
         return np.sort(array, order='mass')
@@ -74,43 +93,47 @@ class MolecularFormulaTest(TestCase):
                 self.assertAlmostEqual(nucIsotopeTable[i][j], protIsotopeTable[i][j])
                 self.assertAlmostEqual(nucIsotopeTable[i][j], uniIsotopeTable[i][j])
         isotopeTable = MolecularFormula('C5H5N5O30P').makeIsotopeTable()
-        self.assertEqual(2, len(getByIndex(isotopeTable,0)))
+        self.assertEqual(2, len(getByIndex(isotopeTable, 0)))
 
     def test_calculate_mono_isotopic(self):
         self.assertAlmostEqual(1223.210776, RNA_formulaDummy.calculateMonoIsotopic(), delta=5 * 10 ** (-6))
         self.assertAlmostEqual(912.375965, peptideFormulaDummy.calculateMonoIsotopic(), delta=5 * 10 ** (-6))
 
     def test_calculate_isotope_pattern(self):
-        RNA_pattern = [(1223.2107777180972, 0.58545685),(1224.2134684, 0.28360589), (1225.21578726, 0.09861313),
+        RNA_pattern = [(1223.2107777180972, 0.58545685), (1224.2134684, 0.28360589), (1225.21578726, 0.09861313),
                        (1226.21815597, 0.02558403), (1227.22044876, 0.00558521)]
         peptide_pattern = [(912.37596571, 0.59104661), (913.37869526, 0.26946771), (914.37874787, 0.1035717),
                            (915.37979107, 0.02834297), (916.38113847, 0.00624432)]
-        uni_pattern = [(174.03917908, 0.92733003),(175.04099458,0.06836688),(176.04296025,0.00412072)]
+        uni_pattern = [(174.03917908, 0.92733003), (175.04099458, 0.06836688), (176.04296025, 0.00412072)]
         self.testIsotopePattern(RNA_pattern, RNA_formulaDummy.calculateIsotopePattern())
-        print(peptideFormulaDummy.calculateIsotopePattern())
         self.testIsotopePattern(peptide_pattern, peptideFormulaDummy.calculateIsotopePattern())
         self.testIsotopePattern(uni_pattern, uniFormulaDummy.calculateIsotopePattern())
-        for i in range(20):
-            molFormulaDummy_i = MolecularFormula({'C':randint(1,50), 'H':randint(1,100),
-                                                  'N':randint(1,50),'O':randint(1,50),
-                                                  'P':randint(0,2),'S':randint(0,2)})
-            print(molFormulaDummy_i.getFormulaDict())
+        for i in range(100):
+            molFormulaDummy_i = MolecularFormula({'C': randint(5, 50), 'H': randint(10, 100),
+                                                  'N': randint(1, 40), 'O': randint(1, 40),
+                                                  'P': randint(0, 2), 'S': randint(0, 2)})
             calcIsotopePattern = molFormulaDummy_i.calculateIsotopePattern()
-            self.assertAlmostEqual(1.0,float(np.sum(calcIsotopePattern['calcInt'])),delta=0.005)
-            self.assertTrue(np.sum(calcIsotopePattern['calcInt'])<1)
-
+            try:
+                self.assertAlmostEqual(1.0, float(np.sum(calcIsotopePattern['calcInt'])), delta=0.009)
+                self.assertTrue(np.sum(calcIsotopePattern['calcInt']) < 1)
+            except AssertionError:
+                print(calcIsotopePattern)
+                raise AssertionError(molFormulaDummy_i.getFormulaDict())
 
     def testIsotopePattern(self, theoIsotopePattern=None, calcIsotopePattern=None):
         if theoIsotopePattern != None:
             theoIsotopePattern = np.array(theoIsotopePattern, dtype=[('m/z', np.float64), ('calcInt', np.float64)])
-            theoIsotopePattern['calcInt'] *= (calcIsotopePattern['calcInt'][0] /theoIsotopePattern['calcInt'][0])
-            if len(theoIsotopePattern) > (len(calcIsotopePattern)+1):
+            theoIsotopePattern['calcInt'] *= (calcIsotopePattern['calcInt'][0] / theoIsotopePattern['calcInt'][0])
+            if len(theoIsotopePattern) > (len(calcIsotopePattern) + 1):
                 raise Exception('Length of calculated isotope pattern to short')
             for i in range(len(theoIsotopePattern)):
-                self.assertAlmostEqual(theoIsotopePattern[i]['m/z'], calcIsotopePattern[i]['m/z'], delta=5*10**(-6))
-                self.assertAlmostEqual(theoIsotopePattern[i]['calcInt'], calcIsotopePattern[i]['calcInt'], delta=5*10**(-6))
-            self.assertAlmostEqual(1.0,float(np.sum(calcIsotopePattern['calcInt'])),delta=0.005)
-            self.assertTrue(np.sum(calcIsotopePattern['calcInt'])<1)
+                self.assertAlmostEqual(theoIsotopePattern[i]['m/z'], calcIsotopePattern[i]['m/z'], delta=5 * 10 ** (-6))
+                self.assertAlmostEqual(theoIsotopePattern[i]['calcInt'], calcIsotopePattern[i]['calcInt'],
+                                       delta=5 * 10 ** (-6))
+            self.assertAlmostEqual(1.0, float(np.sum(calcIsotopePattern['calcInt'])), delta=0.005)
+            self.assertTrue(np.sum(calcIsotopePattern['calcInt']) < 1)
 
     '''def test_calc_isotope_pattern_slowly(self):
         self.fail()'''
+
+
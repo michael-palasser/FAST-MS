12.05.21 tests for LibraryBuilder.py and IsotopePatternRepository.py: silent bugs fixed concerning checking stored isotope patterns (not absolute deviations taken)

src/MolecularFormula.py
@@ -126,6 +126,7 @@ class MolecularFormula(object):
             #print(isotopeTable)
         while(prop/mostAbundant>0.02):              #ToDo:Parameter
             setIsotopeTable(isotopeTable)
+            print(isoPeak, isotopeTable)
             ultrafineStruct = np.array(calculate(isoPeak, isotopeTable))
             prop = np.sum(ultrafineStruct[:,1])
             M_iso = np.sum(ultrafineStruct[:,0]*ultrafineStruct[:,1])/prop
@@ -235,19 +236,23 @@ class MolecularFormula(object):
         self._periodicTable = periodicTableService.getElements(list(self._formulaDict.keys()))
         isotope_pattern = list()
         if self._formulaDict.keys() == {'C', 'H', 'N', 'O', 'P'}:
-            calculate = sf.calculateNuclFineStructure
+            #calculate = sf.calculateNuclFineStructure
+            calculate = calculateNuclFineStructure
             isotopeTable = self.makeNucIsotopeTable()
         elif self._formulaDict.keys() == {'C', 'H', 'N', 'O', 'S'}:
-            calculate = sf.calculatePeptFineStructure
+            #calculate = sf.calculatePeptFineStructure
+            calculate = calculatePeptFineStructure
             isotopeTable = self.makeProteinIsotopeTable()
         else:
-            calculate = sf.calculateFineStructure
+            #calculate = sf.calculateFineStructure
+            calculate = calculateFineStructure
             isotopeTable = self.makeIsotopeTable()
         isotopeTable = isotopeTable.astype([('index',np.int32), ('nr',np.int32), ('nrIso',np.int32),
              ('relAb',np.float64), ('mass',np.float64), ('M+',np.int32)])
         if args and args[0]:
             for isoPeak in range(args[0]):
-                sf.setIsotopeTable(isotopeTable)
+                #sf.setIsotopeTable(isotopeTable)
+                setIsotopeTable(isotopeTable)
                 ultrafineStruct = np.array(calculate(isoPeak, isotopeTable))
                 prop = np.sum(ultrafineStruct[:,1])
                 M_iso = np.sum(ultrafineStruct[:,0]*ultrafineStruct[:,1])/prop

src/repositories/IsotopePatternRepository.py
@@ -3,6 +3,7 @@ import os
 #import sqlite3
 #import time
 #from os.path import join
+import time
 
 import numpy as np
 
@@ -23,7 +24,7 @@ class IsotopePatternRepository(object):
     def findFile(self, settings):
         '''
         Checks if a isotope pattern file exists
-        :param (tuple[str,str,int,str]) settings: settings which are relevant for the filename
+        :param (tuple[str,str,int,str] | list[str,str,int,str]) settings: settings which are relevant for the filename
         :return: (bool) True if file exists
         '''
         if len(settings) == 1:
@@ -32,7 +33,7 @@ class IsotopePatternRepository(object):
                 file += '.csv'
             self.__file = os.path.join(path, 'Fragment_lists',file)
         else:
-            sequName, fragmentation, nrMod, modifications = settings[0], settings[1], settings[2], settings[3],
+            sequName, fragmentation, nrMod, modifications = settings[0], settings[1], settings[2], settings[3]
             if modifications == "-" or nrMod == "0":
                 self.__file = os.path.join(path, 'Fragment_lists', '_'.join((sequName, fragmentation + '.csv')))
             else:
@@ -76,11 +77,13 @@ class IsotopePatternRepository(object):
                 raise InvalidIsotopePatternException("", "no data in file")
             isotopePatternDict[name] = np.array(isotopePattern, dtype=[('m/z',np.float64),('calcInt', np.float64)])
         print('Checking correctness of fragment library')
+        start = time.time()
         for fragment in fragmentLibrary:
             if fragment.getName() not in isotopePatternDict:
                 raise InvalidIsotopePatternException(fragment.getName(), "not found in file")
             self.checkEquality(fragment, isotopePatternDict[fragment.getName()])
             fragment.setIsotopePattern(isotopePatternDict[fragment.getName()])
+        print('time',time.time()-start)
         print('done')
         return fragmentLibrary
 
@@ -92,13 +95,14 @@ class IsotopePatternRepository(object):
         :param (ndarray(dtype = [float,float])) savedPattern: isotope pattern from file
         :raises InvalidIsotopePatternException if not equal
         '''
-        newPattern = fragment.getFormula().calcIsotopePatternSlowly(2)
-        for i in range(2):
-            if newPattern[i]['m/z'] - savedPattern[i]['m/z'] > 10 ** (-6):
+        #newPattern = fragment.getFormula().calcIsotopePatternSlowly(3)
+        newPattern = fragment.getFormula().calcIsotopePatternSlowly(3)
+        for i in range(3):
+            if abs(newPattern[i]['m/z'] - savedPattern[i]['m/z']) > 10 ** (-6):
                 raise InvalidIsotopePatternException(fragment.getName(), "mass incorrect " +
                                                      str(newPattern[i]['m/z']) + " != " + str(savedPattern[i]['m/z']))
-            if newPattern[i]['calcInt'] - savedPattern[i]['calcInt'] > 10 ** (-6):
-                print(newPattern)
+            print(newPattern[i]['calcInt'] - savedPattern[i]['calcInt'])
+            if abs(newPattern[i]['calcInt'] - savedPattern[i]['calcInt']) > 10 ** (-6):
                 raise InvalidIsotopePatternException(fragment.getName(), '(' + str(i) + ") relative Abundance incorrect " +
                                                      str(newPattern[i]['calcInt']) + " != " + str(savedPattern[i]['calcInt']))
 

tests/test_fastFunctions.py
@@ -1,9 +1,10 @@
 from random import randint
 from unittest import TestCase
 import numpy as np
+import math
+
 import src.fastFunctions as f
 from scipy.stats import binom, multinomial
-import math
 from src.MolecularFormula import MolecularFormula
 
 '''G = stringToFormula('C5H5N5O',{},1)
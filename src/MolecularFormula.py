17.05.21 silent bugs fixed concerning calculating charge shift by modifications - SpectrumHandler.getModCharge (incorrect if nrMod was higher than 1)

src/MolecularFormula.py
@@ -2,7 +2,7 @@ import copy
 
 from src.FormulaFunctions import stringToFormula
 from src.fastFunctions import *
-import src.simpleFunctions as sf
+#import src.simpleFunctions as sf
 from src.Services import PeriodicTableService
 
 '''
@@ -109,8 +109,8 @@ class MolecularFormula(object):
         Calculates isotope patterns based on molecular formulas
         :return: (ndarray(dtype=[float,float])) isotope pattern1 (structured numpy array: [(mass,relative Abundance)])
         '''
-        mostAbundant=10**(-10)
-        prop = 1
+        #mostAbundant=10**(-10)
+        #prop = 1
         isoPeak=0
         isotope_pattern = list()
         elements = [key for key,val in self._formulaDict.items() if val>0]
@@ -124,16 +124,16 @@ class MolecularFormula(object):
             calculate = calculateFineStructure
             isotopeTable = self.makeIsotopeTable()
             #print(isotopeTable)
-        while(prop/mostAbundant>0.02):              #ToDo:Parameter
+        while(np.sum(np.array(isotope_pattern))<0.995):              #ToDo:Parameter
             setIsotopeTable(isotopeTable)
-            print(isoPeak, isotopeTable)
+            #print(isoPeak, isotopeTable)
             ultrafineStruct = np.array(calculate(isoPeak, isotopeTable))
             prop = np.sum(ultrafineStruct[:,1])
             M_iso = np.sum(ultrafineStruct[:,0]*ultrafineStruct[:,1])/prop
             isotope_pattern.append((M_iso,prop))
             isoPeak += 1
-            if prop > mostAbundant:
-                mostAbundant = prop
+            #if prop > mostAbundant:
+                #mostAbundant = prop
             """if args and args[0]:
                 if isoPeak == args[0]:
                     return np.array(isotope_pattern, dtype=[('m/z', np.float64), ('calcInt', np.float64)])"""
@@ -258,16 +258,16 @@ class MolecularFormula(object):
                 M_iso = np.sum(ultrafineStruct[:,0]*ultrafineStruct[:,1])/prop
                 isotope_pattern.append((M_iso,prop))
         else:
-            mostAbundant=10**(-10)
-            prop = 1
+            #mostAbundant=10**(-10)
+            #prop = 1
             isoPeak=0
-            while (prop / mostAbundant > 0.02):  # ToDo:Parameter
+            while(np.sum(np.array(isotope_pattern))<0.995):              #ToDo:Parameter# ToDo:Parameter
                 setIsotopeTable(isotopeTable)
                 ultrafineStruct = np.array(calculate(isoPeak, isotopeTable))
                 prop = np.sum(ultrafineStruct[:, 1])
                 M_iso = np.sum(ultrafineStruct[:, 0] * ultrafineStruct[:, 1]) / prop
                 isotope_pattern.append((M_iso, prop))
                 isoPeak += 1
-                if prop > mostAbundant:
-                    mostAbundant = prop
+                #if prop > mostAbundant:
+                #    mostAbundant = prop
         return np.array(isotope_pattern, dtype=[('m/z',np.float64),('calcInt', np.float64)])

src/top_down/SpectrumHandler.py
@@ -54,7 +54,7 @@ class SpectrumHandler(object):
             self.__sprayMode = -1
         self.__upperBound=0
         self.precursor = precursor
-        self.__charge = abs(self.__settings['charge'] - self.precursor.getRadicals())
+        self.__charge = self.calcPrecCharge(self.__settings['charge'], self.precursor.getRadicals())
         self._normalizationFactor = None
 
         self.addSpectrum(self.__settings['spectralData'])
@@ -65,6 +65,9 @@ class SpectrumHandler(object):
         self._peaksArrType = np.dtype([('m/z', np.float64), ('relAb', np.float64),
                                        ('calcInt', np.float64), ('error', np.float32), ('used', np.bool_)])
 
+    def calcPrecCharge(self, charge, radicals):
+        return abs(charge - radicals)
+
     def getSpectrum(self, *args):
         '''
         Returns either full or part of spectrum (peak list)
@@ -74,6 +77,9 @@ class SpectrumHandler(object):
         if args and args[1]:
             return self.__spectrum[np.where((self.__spectrum[:, 0] > args[0]) & (self.__spectrum[:, 0] < args[1]))]
         return self.__spectrum
+
+    def setSpectrum(self, spectrum):
+        self.__spectrum = spectrum
 
     def getUpperBound(self):
         return self.__upperBound
@@ -108,10 +114,16 @@ class SpectrumHandler(object):
         :param file: opended csv-file
         :return: (ndarray(dtype=float, ndim=2)) [(m/z, int)]
         '''
+        skip = 0
+        lines = file.readlines()
+        if lines[0].startswith('m/z'):
+            skip = 1
         try:
-            return np.loadtxt(file, delimiter=',', skiprows=1, usecols=[0, 2])
+            #return np.loadtxt(lines, delimiter=',', skiprows=skip, usecols=[0, 2])
+            return np.loadtxt(lines, delimiter=',', skiprows=skip, usecols=[0, 1])
         except IndexError:
-            return np.loadtxt(file, delimiter=';', skiprows=1, usecols=[0, 2])
+            #return np.loadtxt(lines, delimiter=';', skiprows=skip, usecols=[0, 2])
+            return np.loadtxt(lines, delimiter=';', skiprows=skip, usecols=[0, 1])
 
     def addSpectrumFromTxt(self, file):
         '''
@@ -172,16 +184,17 @@ class SpectrumHandler(object):
             currentWindow = self.getPeaksInWindow(self.__spectrum, currentMz, windowSize)
             peaksHigherThanNoise.append((currentMz, currentWindow[np.where(currentWindow > (noise * 5))].size))
             currentMz += 1
+        print(peaksHigherThanNoise)
         peaksHigherThanNoise = np.array(peaksHigherThanNoise)
         windowSize, currentMz = 100 , 1200
         while currentMz < 2500:
             currentWindow = self.getPeaksInWindow(peaksHigherThanNoise, currentMz, windowSize)
-            sumInt = np.sum(currentWindow[:, 1])
-            print(currentMz, sumInt)
-            if sumInt < 5:
+            numPeaks = np.sum(currentWindow[:, 1])
+            print(currentMz, numPeaks)
+            if numPeaks < 5:
                 currentMz += configs['upperBoundTolerance']
                 break
-            elif sumInt < 10:
+            elif numPeaks < 10:
                 currentMz += 2* configs['upperBoundTolerance']
                 break
             currentMz += windowSize / 2
@@ -276,13 +289,10 @@ class SpectrumHandler(object):
         '''
         modCharge = 0
         for mod, charge in self.__properties.getChargedModifications().items():
-            print(mod, charge)
-            if mod in fragment.getModification():
+            if mod[1:] in fragment.getModification():
                 nrMod = 1
                 if len(findall(r"(\d+)"+mod, fragment.getModification())) > 0:
                     nrMod = int(findall(r"(\d+)"+mod, fragment.getModification())[0])
-                print(modCharge, charge, nrMod, self.__sprayMode)
-                print(type(modCharge), type(charge), type(nrMod), type(self.__sprayMode))
                 modCharge += charge * nrMod * self.__sprayMode
         return modCharge
 